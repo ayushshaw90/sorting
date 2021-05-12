9f7148701bba9a67b64ccb564f7519bffd718be1
Ayush Shaw <ayushshaw127@gmail.com>
2021-05-12 12:39:26 +0530

    Added header file

---
+++ sort.h
@@ -0,0 +1,6 @@
+void selection_sort(int* Arr, int length);
+void bubble_sort(int* Arr, int length);
+void Recursive_bubble_sort(int* Arr, int length);
+void insertion_sort(int B[], int length);
+void ins_sort(int A[], int length);
+
