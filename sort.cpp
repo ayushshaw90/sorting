39cbfe34e62c8e9650f406774060497e225226cc
Ayush Shaw <ayushshaw127@gmail.com>
2021-05-13 15:03:10 +0530

    Quicksort function added

--- sort.cpp
+++ sort.cpp
@@ -1,95 +1,156 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void selection_sort(int* B, int length)
+void selection_sort(int *B, int length)
 {
-    int i,j,temp,max;
-    for(i=length-1;i>=0;i--)
+    int i, j, temp, max;
+    for (i = length - 1; i >= 0; i--)
     {
-        max=i;
-        for(j=0;j<=i;j++)
+        max = i;
+        for (j = 0; j <= i; j++)
         {
-            if(*(B+j)>*(B+max))   //equivalent to    if(B[j]>B[max])
+            if (*(B + j) > *(B + max)) //equivalent to    if(B[j]>B[max])
             {
-                max=j;
+                max = j;
             }
         }
-        temp=*(B+i);
-        *(B+i)=*(B+max);
-        *(B+max)=temp;
+        temp = *(B + i);
+        *(B + i) = *(B + max);
+        *(B + max) = temp;
     }
 }
 
 //Bubble sort using pointers
 void bubble_sort(int *B, int length)
 {
-    int i,j,temp;
-    for(i=length-2;i>=0;i--)
+    int i, j, temp;
+    for (i = length - 2; i >= 0; i--)
     {
-        for(j=0;j<=i;j++)
+        for (j = 0; j <= i; j++)
         {
-            if(*(B+j)>*(B+j+1))
+            if (*(B + j) > *(B + j + 1))
             {
-                temp=*(B+j);
-                *(B+j)=*(B+j+1);
-                *(B+j+1)=temp;
+                temp = *(B + j);
+                *(B + j) = *(B + j + 1);
+                *(B + j + 1) = temp;
             }
         }
     }
 }
 
-void Recursive_bubble_sort(int* B, int length)
+void Recursive_bubble_sort(int *B, int length)
 {
-    int i,temp;
-    if(length==1 || length==0)  //base case
-    return;
+    int i, temp;
+    if (length == 1 || length == 0) //base case
+        return;
 
-    for(i=0;i<length-1;i++)  //sorting
+    for (i = 0; i < length - 1; i++) //sorting
     {
-        if(*(B+i)>*(B+i+1))
+        if (*(B + i) > *(B + i + 1))
         {
-            temp=*(B+i);  //swapping
-            *(B+i)=*(B+i+1);
-            *(B+i+1)=temp;
+            temp = *(B + i); //swapping
+            *(B + i) = *(B + i + 1);
+            *(B + i + 1) = temp;
         }
     }
-    Recursive_bubble_sort(B,length-1);  //calling recursive function
+    Recursive_bubble_sort(B, length - 1); //calling recursive function
 }
 //insertion sort using while loop
 void insertion_sort(int B[], int length)
 {
-    int i,j,key,temp;
-    for(i=1;i<=length-1;i++)
+    int i, j, key, temp;
+    for (i = 1; i <= length - 1; i++)
     {
-        key=B[i];
-        j=i-1;
-        while(j>=0&&B[j]>key)
+        key = B[i];
+        j = i - 1;
+        while (j >= 0 && B[j] > key)
         {
-            temp=B[j+1];
-            B[j+1]=B[j];
-            B[j]=temp;
+            temp = B[j + 1];
+            B[j + 1] = B[j];
+            B[j] = temp;
             j--;
         }
-
     }
 }
 //insertion sort using for loop
 void ins_sort(int A[], int length)
 {
-    int i,j,key,temp;
-    for(i=1;i<length;i++)
+    int i, j, key, temp;
+    for (i = 1; i < length; i++)
     {
-        key=A[i];
-        for(j=i-1;j>=0;j--)
+        key = A[i];
+        for (j = i - 1; j >= 0; j--)
         {
-            if(A[j]<=key)
-            break;
+            if (A[j] <= key)
+                break;
             else
             {
-                temp=A[j];    //Swapping values
-                A[j]=A[j+1];
-                A[j+1]=temp;
+                temp = A[j]; //Swapping values
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
             }
         }
+    }
+}
+int partition(int A[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int i = low ;
+        int j = high + 1;
+        int key = A[low], temp;
+        while (i < j)
+        {
+            do
+            {
+                i++;
+            } while ((A[i] < key)&&(i<=high));
+            do
+            {
+                j--;
+            } while ((A[j] > key)&&(j>0));
+            if (i < j)
+            {
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+            else
+            {
+                A[low] = A[j];
+                A[j] = key;
+                return j;
+            }
+        }
+    }
+    else
+    {
+        return (-1);
+    }
+}
+void quicksort(int A[], int low, int high)
+{
+    int x;
+    if (high > low)
+    {
+        x = partition(A, low, high);
+        if (x > 0)
+        {
+            quicksort(A, low, x - 1);
+            quicksort(A, x + 1, high);
+        }
+    }
+}
+int main()
+{
+    int i;
+    int Arr[] = {4,2,99, 3,65,120,10,8, 2, 85,1,99,85,85};
+    int length = sizeof(Arr) / sizeof(int);
+    quicksort(Arr, 0, length - 1);
+    cout << "The array elements are" << endl;
+    for (i = 0; i < length; i++)
+    {
+        cout << Arr[i] << endl;
     }
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
-void selection_sort(int* Arr, int length);
-void bubble_sort(int* Arr, int length);
-void Recursive_bubble_sort(int* Arr, int length);
-void insertion_sort(int B[], int length);
-void ins_sort(int A[], int length);
-
+void selection_sort(int* Arr, int length);      //selection sort of integer array
+void bubble_sort(int* Arr, int length);          //bubble sort of integer array
+void Recursive_bubble_sort(int* Arr, int length); //Bubble sort of integer array using bubble sort technique
+void insertion_sort(int B[], int length);         //insetion sort of integer array using while loop
+void ins_sort(int A[], int length);             //insertion sort of integer array using for loop
+void quicksort(int A[], int low, int high);
